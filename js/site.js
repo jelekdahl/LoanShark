11ceccfdda7136d51b208f3926990431a81a359f
jelekdahl <145658864+jelekdahl@users.noreply.github.com>
2023-10-23 08:18:07 -0700

    Initial commit

---
+++ js/site.js
@@ -0,0 +1,13 @@
+function displayMessage() {
+  //get the text out of our input and assign it to a variable
+  let msg = document.getElementById('message').value;
+
+  //alert('Hello Guardian Cohort!');
+  Swal.fire(
+    {
+      backdrop: false,
+      title: 'APP NAME',
+      text: msg, // <-- replace this text with that variable
+    }
+  );
+}
